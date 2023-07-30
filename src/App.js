4aec5b90c4db8f75969ed3509bdfad024525ac40
Elisee-des <esabidani@gmail.com>
2023-07-30 11:24:18 +0000

    AuthGuard pour controller l'authentification

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PublicRouter from "@/pages/Public/PublicRouter";
 import AdminRouter from "@/pages/Admin/AdminRouter";
 import AuthRouter from "@/pages/Auth/AuthRouter";
+import AuthGuard from "@/_helpers/AuthGuard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<PublicRouter />} />
-        <Route path="/admin/*" element={<AdminRouter />} />
+        <Route
+          path="/admin/*"
+          element={
+            <AuthGuard>
+              <AdminRouter />
+            </AuthGuard>
+          }
+        />
         <Route path="/auth/*" element={<AuthRouter />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -3,91 +3,94 @@ import React from "react";
 const Login = () => {
   return (
     <main>
-      <div class="container">
-        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                <div class="d-flex justify-content-center py-4">
+      <div className="container">
+        <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                <div className="d-flex justify-content-center py-4">
                   <a
                     href="index.html"
-                    class="logo d-flex align-items-center w-auto"
+                    className="logo d-flex align-items-center w-auto"
                   >
                     <img src="assets/img/logo.png" alt="" />
-                    <span class="d-none d-lg-block">Coktail App</span>
+                    <span className="d-none d-lg-block">Coktail App</span>
                   </a>
                 </div>
 
-                <div class="card mb-3">
-                  <div class="card-body">
-                    <div class="pt-4 pb-2">
-                      <h5 class="card-title text-center pb-0 fs-4">
+                <div className="card mb-3">
+                  <div className="card-body">
+                    <div className="pt-4 pb-2">
+                      <h5 className="card-title text-center pb-0 fs-4">
                         Login to Your Account
                       </h5>
-                      <p class="text-center small">
+                      <p className="text-center small">
                         Enter your username & password to login
                       </p>
                     </div>
 
-                    <form class="row g-3 needs-validation" novalidate>
-                      <div class="col-12">
-                        <label for="yourUsername" class="form-label">
+                    <form className="row g-3 needs-validation" novalidate>
+                      <div className="col-12">
+                        <label for="yourUsername" className="form-label">
                           Username
                         </label>
-                        <div class="input-group has-validation">
-                          <span class="input-group-text" id="inputGroupPrepend">
+                        <div className="input-group has-validation">
+                          <span
+                            className="input-group-text"
+                            id="inputGroupPrepend"
+                          >
                             @
                           </span>
                           <input
                             type="text"
                             name="username"
-                            class="form-control"
+                            className="form-control"
                             id="yourUsername"
                             required
                           />
-                          <div class="invalid-feedback">
+                          <div className="invalid-feedback">
                             Please enter your username.
                           </div>
                         </div>
                       </div>
 
-                      <div class="col-12">
-                        <label for="yourPassword" class="form-label">
+                      <div className="col-12">
+                        <label for="yourPassword" className="form-label">
                           Password
                         </label>
                         <input
                           type="password"
                           name="password"
-                          class="form-control"
+                          className="form-control"
                           id="yourPassword"
                           required
                         />
-                        <div class="invalid-feedback">
+                        <div className="invalid-feedback">
                           Please enter your password!
                         </div>
                       </div>
 
-                      <div class="col-12">
-                        <div class="form-check">
+                      <div className="col-12">
+                        <div className="form-check">
                           <input
-                            class="form-check-input"
+                            className="form-check-input"
                             type="checkbox"
                             name="remember"
                             value="true"
                             id="rememberMe"
                           />
-                          <label class="form-check-label" for="rememberMe">
+                          <label className="form-check-label" for="rememberMe">
                             Remember me
                           </label>
                         </div>
                       </div>
-                      <div class="col-12">
-                        <button class="btn btn-primary w-100" type="submit">
+                      <div className="col-12">
+                        <button className="btn btn-primary w-100" type="submit">
                           Login
                         </button>
                       </div>
-                      <div class="col-12">
-                        <p class="small mb-0">
+                      <div className="col-12">
+                        <p className="small mb-0">
                           Don't have account?{" "}
                           <a href="pages-register.html">Create an account</a>
                         </p>
@@ -96,7 +99,7 @@ const Login = () => {
                   </div>
                 </div>
 
-                {/* <div class="credits">
+                {/* <div className="credits">
                     Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                   </div> */}
               </div>
